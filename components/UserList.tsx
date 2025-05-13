091479409d3b0fe8498260cfa234eb4fca73a53c
YamazakiTomoya <tomoya.yamazaki@fox-hound.jp>
2025-05-13 15:26:55 +0900

    タスク2-3：UserCardコンポーネントとDeleteUserButtonの統合

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Grid } from "@mui/material";
 import UserCard from "./UserCard";
 import { User } from "../types/User";
@@ -8,11 +8,17 @@ interface UserListProps {
 }
 
 const UserList: React.FC<UserListProps> = ({ users }) => {
+  const [userList, setUserList] = useState<User[]>(users);
+
+  const handleDelete = (userId: number) => {
+    setUserList((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+  };
+
   return (
     <Box sx={{ mt: 4 }}>
-      {users.map((user) => (
+      {userList.map((user) => (
         <Grid item xs={12} sm={6} md={4} key={user.id}>
-          <UserCard user={user} />
+          <UserCard user={user} onDelete={handleDelete} />
         </Grid>
       ))}
     </Box>
