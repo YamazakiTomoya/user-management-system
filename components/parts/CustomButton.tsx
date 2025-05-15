6afbe01a84a878f110e9a04be42ae437e2d9d0ef
YamazakiTomoya <tomoya.yamazaki@fox-hound.jp>
2025-05-15 11:32:48 +0900

    タスク4CustomButtonストーリー追加

--- components/parts/CustomButton.tsx
+++ components/parts/CustomButton.tsx
@@ -7,20 +7,37 @@ interface CustomButtonProps extends ButtonProps {
 
 const CustomButton: React.FC<CustomButtonProps> = ({ variantType = 'primary', ...props }) => {
   let sxStyle = {};
+  let hoverStyle = {};
 
   switch (variantType) {
     case 'secondary':
-      sxStyle = { backgroundColor: 'purple', color: 'white'};
+      sxStyle = { backgroundColor: 'purple', color: 'white' };
+      hoverStyle = { backgroundColor: '#5e005e' };
       break;
     case 'danger':
-      sxStyle = { backgroundColor: 'red', color: 'white'};
+      sxStyle = { backgroundColor: 'red', color: 'white' };
+      hoverStyle = { backgroundColor: '#b20000' };
       break;
     default:
-      sxStyle = { backgroundColor: 'blue', color: 'white'};
+      sxStyle = { backgroundColor: 'blue', color: 'white' };
+      hoverStyle = { backgroundColor: '#003399' };
   }
 
   return (
-    <Button sx={sxStyle} variant="contained" {...props}>
+    <Button
+      sx={{
+        ...sxStyle,
+        transition: 'transform 0.1s, background-color 0.2s',
+        '&:hover': {
+          ...hoverStyle,
+        },
+        '&:active': {
+          transform: 'scale(0.95)',
+        },
+      }}
+      variant="contained"
+      {...props}
+    >
       {props.children}
     </Button>
   );
